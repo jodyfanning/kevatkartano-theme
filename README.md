a499adbac62bad744d91d219d1e01820b21754bd
Jody Fanning <jodyfanning@gmail.com>
2013-11-17 20:49:45 +0200

    Fix few minor issues
    * Indent comments area
    * Add meta headers
    * Add favicon

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ Kevätkartano theme
 ==================
 
 I'm a WordPress theme based on `_s`, or `underscores`.
+
+Changes
+------------------
+
+Version 1.1
+* Indent comments area
+* Add meta headers
+* Add favicon
+
+Version 1.0
+* Initial version
--- header.php
+++ header.php
@@ -10,16 +10,20 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="application-name" content="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
+    <meta name="msapplication-starturl" content="/" />
+    <meta name="msapplication-navbutton-color" content="green" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="shortcut icon" type="image/ico" href="<?php echo esc_attr( bloginfo( 'template_directory' ) ); ?>/favicon.ico" />
+	<!--[if lt IE 9]>
+		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+	<![endif]-->
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
